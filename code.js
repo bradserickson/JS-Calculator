6796fe15364359d6209d1c9b3a549e66d38f4af2
bradserickson <118328953+bradserickson@users.noreply.github.com>
2023-02-07 14:38:13 -0700

    Add EventListener for each number button

--- code.js
+++ code.js
@@ -21,15 +21,47 @@ const buttMinus = document.querySelector('#thirdline > button:nth-child(4)');
 const butt1 = document.querySelector('#fourthline > button:nth-child(1)');
 const butt2 = document.querySelector('#fourthline > button:nth-child(2)');
 const butt3 = document.querySelector('#fourthline > button:nth-child(3)');
-const buttPlus = document.querySelector('fourthline > button:nth-child(4)');
+const buttPlus = document.querySelector('#fourthline > button:nth-child(4)');
 
 //variables for the fifth line
 const butt0 = document.querySelector('#fifthline > button:nth-child(1)');
 const buttPeriod = document.querySelector('#fifthline > button:nth-child(2)');
 const buttEqual = document.querySelector('#fifthline > button:nth-child(3)');
 
-butt1.addEventListener('click',function(){
-    screen += butt1.innerText;
-    console.log(screen);
+var total = 0;
 
-})
+butt1.addEventListener('click',function(){
+    screen.innerText += butt1.innerText;
+});
+
+butt2.addEventListener('click',function(){
+    screen.innerText += butt2.innerText;
+});
+
+butt3.addEventListener('click',function(){
+    screen.innerText += butt3.innerText;
+});
+
+butt4.addEventListener('click',function(){
+    screen.innerText += butt4.innerText;
+});
+
+butt5.addEventListener('click',function(){
+    screen.innerText += butt5.innerText;
+});
+
+butt6.addEventListener('click',function(){
+    screen.innerText += butt6.innerText;
+});
+
+butt7.addEventListener('click',function(){
+    screen.innerText += butt7.innerText;
+});
+
+butt8.addEventListener('click',function(){
+    screen.innerText += butt8.innerText;
+});
+
+butt9.addEventListener('click',function(){
+    screen.innerText += butt9.innerText;
+});
