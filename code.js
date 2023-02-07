bf6c7a9d530f9bafd4427bca59c49067b27b7c15
bradserickson <118328953+bradserickson@users.noreply.github.com>
2023-02-07 10:56:20 -0700

    Add semicolons and array
    Added semicolons missing at the end of each const line, added an array
    containing all buttons.

--- code.js
+++ code.js
@@ -1,29 +1,31 @@
 var screen = document.querySelector("#displayBox");
 //variables for the first line
-const buttAc = document.querySelector('#firstline > button:nth-child(1)')
-const buttPosNeg = document.querySelector('#firstline > button:nth-child(2)')
-const buttPercent = document.querySelector('#firstline > button:nth-child(3)')
-const buttDiv = document.querySelector('#firstline > button:nth-child(4)')
+const buttAc = document.querySelector('#firstline > button:nth-child(1)');
+const buttPosNeg = document.querySelector('#firstline > button:nth-child(2)');
+const buttPercent = document.querySelector('#firstline > button:nth-child(3)');
+const buttDiv = document.querySelector('#firstline > button:nth-child(4)');
 
 //variables for the second line
-const butt7 = document.querySelector('#secondline > button:nth-child(1)')
-const butt8 = document.querySelector('#secondline > button:nth-child(2)')
-const butt9 = document.querySelector('#secondline > button:nth-child(3)')
-const buttX = document.querySelector('#secondline > button:nth-child(4)')
+const butt7 = document.querySelector('#secondline > button:nth-child(1)');
+const butt8 = document.querySelector('#secondline > button:nth-child(2)');
+const butt9 = document.querySelector('#secondline > button:nth-child(3)');
+const buttX = document.querySelector('#secondline > button:nth-child(4)');
 
 //variables for the third line
-const butt4 = document.querySelector('#thirdline > button:nth-child(1)')
-const butt5 = document.querySelector('#thirdline > button:nth-child(2)')
-const butt6 = document.querySelector('#thirdline > button:nth-child(3)')
-const buttMinus = document.querySelector('#thirdline > button:nth-child(4)')
+const butt4 = document.querySelector('#thirdline > button:nth-child(1)');
+const butt5 = document.querySelector('#thirdline > button:nth-child(2)');
+const butt6 = document.querySelector('#thirdline > button:nth-child(3)');
+const buttMinus = document.querySelector('#thirdline > button:nth-child(4)');
 
 //variables for the fourth line
-const butt1 = document.querySelector('fourthline > button:nth-child(1)')
-const butt2 = document.querySelector('fourthline > button:nth-child(2)')
-const butt3 = document.querySelector('fourthline > button:nth-child(3)')
-const buttPlus = document.querySelector('fourthline > button:nth-child(4)')
+const butt1 = document.querySelector('fourthline > button:nth-child(1)');
+const butt2 = document.querySelector('fourthline > button:nth-child(2)');
+const butt3 = document.querySelector('fourthline > button:nth-child(3)');
+const buttPlus = document.querySelector('fourthline > button:nth-child(4)');
 
 //variables for the fifth line
-const butt0 = document.querySelector('fifthline > button.nth-child(1)')
-const buttPeriod = document.querySelector('fifthline > button.nth-child(1)')
-const buttEqual = document.querySelector('fifthline > button.nth-child(1)')
+const butt0 = document.querySelector('fifthline > button.nth-child(1)');
+const buttPeriod = document.querySelector('fifthline > button.nth-child(1)');
+const buttEqual = document.querySelector('fifthline > button.nth-child(1)');
+
+const allButtons = [buttAC, buttPosNeg, buttPercent, buttDiv, butt7, butt8, butt9, buttX, butt4, butt5, butt6, buttMinus, butt1, butt2, butt3, buttPlus, butt0, buttPeriod, buttEqual];
